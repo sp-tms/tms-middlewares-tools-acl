769fffc36bc756a54efffdb1a0924a093c251fb8
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-05-08 23:58:32 +0530

    Initial Commit

---
+++ Acl.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Apps\Tms\Middlewares\Acl;
+
+use System\Base\BaseMiddleware;
+
+class Acl extends BaseMiddleware
+{
+    public function process($data)
+    {
+        //
+    }
+}
